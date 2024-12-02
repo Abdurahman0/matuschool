51e63eedebf5e62330ee548ddbd1f31d12c573bc
Dumbie <wasleyron668@gmail.com>
2024-12-02 18:52:25 +0500

    fixed image imports for production

--- src/data.js
+++ src/data.js
@@ -1,3 +1,11 @@
+import mask from '/public/Mask group.png'
+import star from '/public/Star.png'
+import hat from '/public/Hat.png'
+import person from '/public/Person.png'
+import cake1 from '/public/cake1.png'
+import cake2 from '/public/cake2.png'
+import cake3 from '/public/cake3.png'
+
 export const navlinks = [
 	{ name: 'Акции', href: '/sale' },
 	{ name: 'Отзывы', href: '/coments' },
@@ -6,22 +14,22 @@ export const navlinks = [
 
 export const forYouData = [
 	{
-		icon: '/public/Mask group.png',
+		icon: mask,
 		title: 'Любитель',
 		text: 'Хотите научится готовить вкусные и красивые торты для себя и близких',
 	},
 	{
-		icon: '/public/Star.png',
+		icon: star,
 		title: 'Новичок',
 		text: 'Очень хотите начать, но сомневаетесь, что сможете так же, как и другие кондитеры',
 	},
 	{
-		icon: '/public/Hat.png',
+		icon: hat,
 		title: 'Опытный',
 		text: 'Вы уже опытный кондитер и хотите расширить свой ассортимент',
 	},
 	{
-		icon: '/public/Person.png',
+		icon: person,
 		title: 'Ученик',
 		text: 'Хотите освоить новый вид деятельности и начать на этом зарабатывать',
 	},
@@ -53,12 +61,12 @@ export const aboutData = [
 
 export const imageData = [
 	{
-		image: '/public/cake1.png',
+		image: cake1,
 	},
 	{
-		image: '/public/cake2.png',
+		image: cake2,
 	},
 	{
-		image: '/public/cake3.png',
+		image: cake3,
 	},
 ]
